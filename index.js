670cfea92df690b13a3bae7d6296b9de97a1f236
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-03-18 10:43:51 +0530

    mvp

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+console.log(ulEl);
 
 
 const inputBtn  = document.getElementById("input-btn");
@@ -8,9 +10,24 @@ const inputBtn  = document.getElementById("input-btn");
 inputBtn.addEventListener("click" , function(){
   console.log("button clicked");
   myLeads.push(inputEl.value);
+  inputEl.value = "";
   console.log(myLeads);
-  for(let i=0 ; i<myLeads.length ; i++){
-  console.log(myLeads[i]);
-}
+  renderLeads();
 })
+
+
+function renderLeads(){
+  let listItems = ""
+  for(let i=0 ; i<myLeads.length ; i++){
+
+    listItems += `
+    <li>
+      <a href="${myLeads[i]}" target="_blank">
+        ${myLeads[i]}
+      </a>
+    </li>`;
+  }
+  ulEl.innerHTML = listItems;
+}
+
 
