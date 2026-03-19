f3be685597ed9d1b759959a30d00e1ae5997e5df
GAVIN-DMELLO <196908464+GAVIN-DMELLO@users.noreply.github.com>
2026-03-19 18:11:12 +0530

    final product

--- index.js
+++ index.js
@@ -1,40 +1,80 @@
 
-let myLeads = ["www.example.ccom"];
-
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-console.log(leadsFromLocalStorage);
-
+let myLeads = [];
 const inputEl = document.getElementById("input-el");
-const ulEl = document.getElementById("ul-el");
-console.log(ulEl);
-
-
 const inputBtn  = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+const tabBtn = document.getElementById("tab-btn");
+
+
+
+
+if(leadsFromLocalStorage){
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+  console.log(myLeads);
+}
+
+
+
+
+
+
+tabBtn.addEventListener("click" , function(){
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+  })
+
+  chrome.tabs.query({active: true , currentWindow : true} , function(){
+    console.log(tabs)
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads" , JSON.stringify(myLeads));
+    render(myLeads);
+  })
+
+})
+
+
+
+
+
 
 inputBtn.addEventListener("click" , function(){
-  console.log("button clicked");
   myLeads.push(inputEl.value);
   inputEl.value = "";
   localStorage.setItem("myLeads" , JSON.stringify(myLeads));
   console.log(myLeads);
-  renderLeads();
-
+  render(myLeads);
   console.log(localStorage.getItem("myLeads"));
 })
 
 
-function renderLeads(){
+function render(leads){
   let listItems = ""
-  for(let i=0 ; i<myLeads.length ; i++){
+  for(let i=0 ; i<leads.length ; i++){
 
     listItems += `
     <li>
-      <a href="${myLeads[i]}" target="_blank">
-        ${myLeads[i]}
+      <a href="${leads[i]}" target="_blank">
+        ${leads[i]}
       </a>
     </li>`;
   }
   ulEl.innerHTML = listItems;
 }
+
+
+
+
+deleteBtn.addEventListener("dblclick" , function(){
+  console.log("button clicked delete");
+  localStorage.clear();
+  myLeads = [];
+  console.log(myLeads);
+  render(myLeads);
+})
 
 
